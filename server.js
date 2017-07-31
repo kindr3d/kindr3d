7618488f86a8c94ce49c3904368f56c7d89045f4
hom3mad3 <micaelarava@gmail.com>
2017-07-31 14:50:38 +0200

    Setup basic api routing

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-//Load express module with `require` directive
+// BASE SETUP
+// =============================================================================
+
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// configure app to use bodyParser()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-//Launch listening server on port 8080
-app.listen(8080, function () {
-  console.log('app listening on port 8080!')
-})
+var port = process.env.PORT || 8080;    // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+var router = express.Router()           // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'hooray! welcome to our api!' })
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router)
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port)
