ff193fa9f3d55939b3af7fe422a2958a704bc6fd
saintsebastian <tilitidam@gmail.com>
2017-08-11 18:46:57 +0200

    feat: nodemon+xo scripts

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### This is our attempt to create a place for people like us and learn a thing or two
 
 ## Ideas
-* ~~learn some hardware by hosting and dockerising the server side ourselves (strikethrough)~~
+* ~~learn some hardware by hosting and dockerising the server side ourselves~~
 * ~~use React, Node.js, Jest, Express to create a bookclub/skillsharing/support network~~
 * setup our own testing suit (TravisCI)
 * document everything we do and write posts about it to help other learners
--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-// BASE SETUP
-// =============================================================================
-
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-var port = process.env.PORT || 8080;    // set our port
+ // set our port
+const port = process.env.PORT || 8080;
 
 // ROUTES FOR OUR API
 // =============================================================================
-
-var router = express.Router()           // get an instance of the express Router
+// get an instance of the express Router
+const router = express.Router();
+app.use('/api', router)
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' })
+router.get('/', (req, res) => {
+  res.json({message: 'hooray! welcome to our api!'})
 });
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router)
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port)
+console.log('Magic happens on port ' + port + '/api');
