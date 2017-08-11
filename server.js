e20e11993d43daaeee03c564d48dc8d7fc6b36f8
hom3mad3 <micaelarava@gmail.com>
2017-08-11 18:53:58 +0200

    Setup testing with mocha chai sinon

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
 // BASE SETUP
-// =============================================================================
 
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-//connect to the database
+// connect to the database
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test', {
   useMongoClient: true,
 });
 
-
 // configure app to use bodyParser()
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 var port = process.env.PORT || 8080;    // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
+// ROUTES FOR THE API
 
-var router = express.Router()           // get an instance of the express Router
+var router = express.Router()
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// just a test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' })
 });
 
 // more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// REGISTER THE ROUTES
 app.use('/api', router)
 
 // START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port)
