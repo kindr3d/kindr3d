e2a02deee9ce0f5424cd262315385ed78fa3e38b
saintsebastian <tilitidam@gmail.com>
2017-08-14 12:27:24 +0200

    test: user route initial post test

--- test/routes/userRoutes.test.js
+++ test/routes/userRoutes.test.js
@@ -1,7 +1,43 @@
-const {app, router} = require('../../server');
-const supertest = require('supertest');
+const request = require('supertest');
+const assert = require('chai').assert;
 const mongoose = require('mongoose');
+const sinon = require('sinon');
+
+const {app, router} = require('../../server');
 const config = require('../../config');
+const User = require('../../lib/models/user');
 
 process.env.NODE_ENV = 'test';
+app.set('env', process.env.NODE_ENV);
 app.set('dbUrl', config.db[process.env.NODE_ENV]);
+
+describe('Route /user', () => {
+  before(() => {
+    mongoose.connect(app.get('dbUrl'), {
+      useMongoClient: true,
+    });
+  });
+
+  after(() => {
+    for (var i in mongoose.connection.collections) {
+       mongoose.connection.collections[i].remove();
+    }
+    mongoose.disconnect();
+  });
+
+  it('creats user on post', (done) => {
+    const name = 'Testuser';
+    const createSpy = sinon.spy(User, 'create')
+    request(app)
+      .post('/api/users')
+      .set('Accept', /json/)
+      .send({name})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((error,res) => {
+        assert.equal(res.body.name, name);
+        sinon.assert.calledOnce(createSpy);
+        done();
+      })
+  });
+});
